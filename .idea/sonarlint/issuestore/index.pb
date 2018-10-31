cba7bb6a9abb57d9e523005035b87fc3785adc7e
bBentein <Brent.Bentein@ebo-enterprises.com>
2018-10-31 09:05:29 +0100

    added testClass

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,8 @@
+
+J
+src/test/java/NewTest.java,e/e/ee6c30bef97b5207cfd4ed5cec82a0a82347457a
+7
+pom.xml,4/4/442292b8a7efeabbe4cc176709b833b1792140ec
+:
+
+testng.xml,5/2/522a7a4e21649af06297e31a9e33f13bb4fd3022
---
+++ src/test/java/NewTest.java
@@ -0,0 +1,25 @@
+import junit.framework.Assert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class NewTest {
+
+    private WebDriver driver;
+    @Test
+    public void testEasy() {
+        driver.get("http://demo.guru99.com/test/guru99home/");
+        String title = driver.getTitle();
+        Assert.assertTrue(title.contains("Demo Guru99 Page"));
+    }
+    @BeforeTest
+    public void beforeTest() {
+        driver = new ChromeDriver();
+    }
+    @AfterTest
+    public void afterTest() {
+        driver.quit();
+    }
+}
